9e5353686bb0b3d4119606b8200398db4a6aaf15
Chrystian Rodolfo <chrystian@kenzie.com.br>
2024-04-26 10:27:28 -0300

    adicionado configuração cors

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 import "express-async-errors";
+import cors from "cors";
 
 import { handleErrors } from "./middlewares";
 import { initRoutes } from "./routers";
@@ -11,6 +12,7 @@ const app = express();
 
 // inicializar os processos necessarios para roda a aplicação
 export const initApp = () => {
+  app.use(cors());
   app.use(express.json());
   initRoutes(app);
   initJwtEnvVars();
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from "./app";
 import { initApp } from "./app";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Application is running on port: ${PORT}`);
